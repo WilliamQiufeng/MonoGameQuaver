98917cfd6c960d389333fa1ca363ed245a23fda9
Ivan Molodetskikh <yalterz@gmail.com>
2020-06-09 18:13:30 +0800

    Hook up SDL's clipboard access functions

--- MonoGame.Framework/Game.cs
+++ MonoGame.Framework/Game.cs
@@ -1228,5 +1228,27 @@ namespace Microsoft.Xna.Framework
                 return object.Equals(Item, ((AddJournalEntry<T>)obj).Item);
             }
         }
+
+        /// <summary>
+        ///     Gets the clipboard contents if Platform is SDL.
+        /// </summary>
+        /// <returns></returns>
+        public string GetClipboardText()
+        {
+            if (Platform is SdlGamePlatform)
+                return Sdl.GetClipboardText();
+
+            return string.Empty;
+        }
+
+        /// <summary>
+        ///     Sets the clipboard contents if Platform is SDL.
+        /// </summary>
+        /// <returns></returns>
+        public void SetClipboardText(string text)
+        {
+            if (Platform is SdlGamePlatform)
+                Sdl.SetClipboardText(text);
+        }
     }
 }
